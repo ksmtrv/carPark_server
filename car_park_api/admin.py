3b2d3e4e3e190f5e7aec8f54667a9bcd0246e81d
ksmtrv <smotrova-kristina@mail.ru>
2023-04-13 23:55:48 +0300

    add User model

--- car_park_api/admin.py
+++ car_park_api/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+from .forms import UserAdminCreationForm, UserAdminChangeForm
 
-# Register your models here.
+admin.site.unregister(Group)
+User = get_user_model()
+
+class UserAdmin(BaseUserAdmin):
+    add_form = UserAdminCreationForm
+
+    list_display = ('first_name', 'last_name', 'phone_number', 'email',  'is_staff')
+    ordering = ('phone_number',)
+    fieldsets = (
+        (None, {
+            'fields': ('first_name', 'last_name', 'patronymic', 'phone_number', 'email', 'is_driver')
+        }),
+        ("System info", {
+            'classes': ('collapse',),
+            'fields': ('is_staff', 'is_superuser', 'is_active')
+        })
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'phone_number',
+                'first_name',
+                'last_name',
+                'patronymic',
+                'email',
+                'secret_word',
+                'is_driver',
+                'password',
+                'password_2'
+            ),
+        }),
+    )
+
+
+admin.site.register(User, UserAdmin)
+
--- car_park_api/models.py
+++ car_park_api/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from phonenumber_field.modelfields import PhoneNumberField
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, phone_number, secret_word, password, **extra_fields):
+        if not phone_number:
+            raise ValueError('The given phone_number must be set')
+
+        user = self.model(phone_number=phone_number, secret_word=secret_word, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, phone_number, secret_word=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(phone_number, secret_word, password, **extra_fields)
+
+    def create_superuser(self, phone_number, secret_word=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(phone_number, secret_word, password, **extra_fields)
+
+    def with_perm(self, perm, is_active=True, include_superusers=True, backend=None, obj=None):
+        if backend is None:
+            backends = auth._get_backends(return_tuples=True)
+            if len(backends) == 1:
+                backend, _ = backends[0]
+            else:
+                raise ValueError(
+                    'You have multiple authentication backends configured and '
+                    'therefore must provide the `backend` argument.'
+                )
+        elif not isinstance(backend, str):
+            raise TypeError(
+                'backend must be a dotted import path string (got %r).'
+                % backend
+            )
+        else:
+            backend = auth.load_backend(backend)
+        if hasattr(backend, 'with_perm'):
+            return backend.with_perm(
+                perm,
+                is_active=is_active,
+                include_superusers=include_superusers,
+                obj=obj,
+            )
+        return self.none()
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    phone_number = PhoneNumberField(_('Номер телефона'), null=False, blank=False, unique=True)
+    email = models.EmailField(_('Email'), null=True, blank=True, unique=False)
+    secret_word = models.CharField(_('Секретное слово'), max_length=150, blank=False, null=False, unique=False)
+    is_driver = models.BooleanField(
+        _('driver status'),
+        default=False,
+        help_text=_('.'),
+    )
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+
+    first_name = models.CharField(_('Имя'), max_length=150, blank=True)
+    last_name = models.CharField(_('Фамилия'), max_length=150, blank=True)
+    patronymic = models.CharField(_('Отчество'), max_length=150, blank=True)
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['secret_word']
+
+    objects = UserManager()
+
+    def get_fullname(self):
+        return f"{self.last_name} {self.first_name[0]}. {self.patronymic[0]}. "
+
+    def __str__(self):
+        return str(self.phone_number)
